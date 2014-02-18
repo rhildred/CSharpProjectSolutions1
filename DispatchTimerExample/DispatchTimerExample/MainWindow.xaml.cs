975d5176b99a1f9e157432d28a3f3bb4bba976be
rhildred <rhildred@gmail.com>
2014-02-18 10:01:57 -0500

    added dispatch timer example

---
+++ DispatchTimerExample/DispatchTimerExample/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace DispatchTimerExample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DispatcherTimer dt = new DispatcherTimer();
+        public MainWindow()
+        {
+            InitializeComponent();
+            dt.Tick += new EventHandler(enterFrame);
+            dt.Interval = new TimeSpan(0, 0, 2);
+            dt.Start();
+
+        }
+        void enterFrame(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            byte[] vals = new byte[3];
+            rnd.NextBytes(vals);
+            Color c = Color.FromRgb(vals[0], vals[1], vals[2]);
+            this.Background = new SolidColorBrush(c);
+        }
+    }
+}
