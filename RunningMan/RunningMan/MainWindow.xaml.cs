72f6ec1bcedca4e311ec59986f37e315d39131e0
Rich Hildred <rhildred@gmail.com>
2014-02-23 11:16:06 -0500

    actually animated man

--- RunningMan/RunningMan/MainWindow.xaml.cs
+++ RunningMan/RunningMan/MainWindow.xaml.cs
@@ -32,21 +32,37 @@ namespace RunningMan
         // copy from source to destination bitmap which needs to be writeable
         BitmapImage sourceBitmap = new BitmapImage(new Uri("pack://application:,,,/filmstrip.png"));
         WriteableBitmap destinationBitmap = null;
+        const int nFrameHeight = 504, nFrames = 9;
+        int dx = 8;
+        int nFrame = 0, x = 0;
         private void enterFrame(object sender, EventArgs e)
         {
-            if (destinationBitmap == null)
-            {
-                // then this is the first time and we need to set up destination
-                destinationBitmap = new WriteableBitmap((int)DestinationImage.Width, (int)DestinationImage.Height, 
-                    96, 96, PixelFormats.Bgra32, null);
-                DestinationImage.Source = destinationBitmap;
-            }
+            destinationBitmap = new WriteableBitmap((int)DestinationImage.Width, (int)DestinationImage.Height, 
+                96, 96, PixelFormats.Pbgra32, null);
+            DestinationImage.Source = destinationBitmap;
             // make a copy buffer
             int nRowBytes = sourceBitmap.PixelWidth * sourceBitmap.Format.BitsPerPixel / 8;
-            byte[] buffer = new byte[nRowBytes * 504];
+            byte[] buffer = new byte[nRowBytes * nFrameHeight];
             // copy through buffer 
-            sourceBitmap.CopyPixels(new Int32Rect(0, 0, sourceBitmap.PixelWidth, 504), buffer, nRowBytes, 0);
-            destinationBitmap.WritePixels(new Int32Rect(0, 0, sourceBitmap.PixelWidth, 504), buffer, nRowBytes, 0);
+            sourceBitmap.CopyPixels(new Int32Rect(0, nFrame * nFrameHeight, sourceBitmap.PixelWidth, nFrameHeight), buffer, nRowBytes, 0);
+            destinationBitmap.WritePixels(new Int32Rect(x, 0, sourceBitmap.PixelWidth, nFrameHeight), buffer, nRowBytes, 0);
+            if (++nFrame == nFrames) nFrame = 0;
+            x += dx;
+            if (x > (int)DestinationImage.Width - sourceBitmap.PixelWidth) x = 0;
+        }
+
+        private void changeSpeedAndDirection(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Up:
+                    dx++;
+                    break;
+                case Key.Down:
+                    dx--;
+                    if (dx == 0) dx = 8;
+                    break;
+            }
         }
     }
 }
